7041ee1428a2f2b848009dd91e81951b1a0dbef0
Joonghyeon Kim <gotka0211@kookmin.ac.kr>
2020-11-04 19:02:46 +0900

    gui에서 그래프 출력

---
+++ AD/graphex.py
@@ -0,0 +1,61 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setupUI()
+
+    def setupUI(self):
+        self.setGeometry(200, 200,800, 500)
+        self.setWindowTitle("PyChart Viewer v0.1")
+        self.setWindowIcon(QIcon('icon.png'))
+
+        self.lineEdit = QLineEdit()
+        self.pushButton = QPushButton("차트그리기")
+        self.pushButton.clicked.connect(self.buttonClicked)
+
+        self.fig = plt.Figure()
+        self.canvas = FigureCanvas(self.fig)
+
+        leftLayout = QVBoxLayout()
+        leftLayout.addWidget(self.canvas)
+
+        # Right Layout
+        rightLayout = QVBoxLayout()
+        rightLayout.addWidget(self.lineEdit)
+        rightLayout.addWidget(self.pushButton)
+        rightLayout.addStretch(1)
+
+        layout = QHBoxLayout()
+        layout.addLayout(leftLayout)
+        layout.addLayout(rightLayout)
+
+        layout.setStretchFactor(leftLayout, 1)
+        layout.setStretchFactor(rightLayout, 0)
+
+        self.setLayout(layout)
+
+    def buttonClicked(self):
+        print(self.lineEdit.text())
+
+        x=np.arange(20)
+        y=np.random.rand(20)
+
+        ax = self.fig.add_subplot()
+        ax.cla()
+
+        ax.plot(x,y)
+        ax.grid()
+        self.canvas.draw()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    app.exec_()
