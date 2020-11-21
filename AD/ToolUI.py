f56922a5af94d4fe203a8ad701901b5f91883d44
Kim-Yul <65997015+Kim-Yul@users.noreply.github.com>
2020-11-21 21:29:25 +0900

    실행 안됨 just test
    실행은 안 되지만, 현재 이렇게 하려고 틀 적은 거 업로드

--- AD/ToolUI.py
+++ AD/ToolUI.py
@@ -3,6 +3,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
 from Button import Button
+import Canvas
 
 
 class ToolUI(QWidget):
@@ -35,19 +36,28 @@ class ToolUI(QWidget):
 
         #색상
         layout.addWidget(QLabel('전경색: '))
-        layout.addWidget(Button('1',self.buttonClicked))
+        layout.addWidget(Button('1', self.foregroundColor()))
         layout.addWidget(QLabel('배경색: '))
-        layout.addWidget(Button('2',self.buttonClicked))
+        layout.addWidget(Button('2',self.backgroungColor()))
 
     def save(self):
-        fpath, _ = QFileDialog.getSaveFileName(self, 'Save Image', '', "PNG(*.png);;JPEG(*.jpg *.jpeg);;All Files(*.*) ")
+        fpath, _ = QFileDialog.getSaveFileName(self, 'Save Image', '', "PNG(*.png) ")
 
         if fpath:
-            self.image.save(fpath)
+            self.canvas.image.save(fpath + '.png')
+            # 혹시 다른 확장자로 바꾸거나 .png.png 로 저장되는 걸 수정하고 싶으면 이 부분 고치
+
 
     def clear(self):
         self.canvas.image.fill(Qt.white)
         self.update()
+
+
+    def foregroundColor(self):
+        Canvas.ChangedColor(Qt.red)
+
+    def backgroungColor(self):
+        Canvas.ChangedColor(Qt.white)
 
 
 
