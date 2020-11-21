f027c342083b1f016a6bccf3d9e6ef8598a2b037
Kim-Yul <65997015+Kim-Yul@users.noreply.github.com>
2020-11-22 03:19:06 +0900

    전체적인 기능 수정
    그리기, 지우개, 그리고 두 기능의 굵기를 구현할 수 있도록 수정함. 또한, 기존에 clear가 제대로 작동하지 않았던 것을 수정하였으며 직선긋기를 할 수 있게끔 기본 틀을 짜둠

--- AD/ToolUI.py
+++ AD/ToolUI.py
@@ -3,7 +3,6 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
 from Button import Button
-import Canvas
 
 
 class ToolUI(QWidget):
@@ -11,6 +10,10 @@ class ToolUI(QWidget):
         super().__init__(parent)
         self.canvas = canvas
         self.painter = painter
+        self.save_eraser_size = 5
+        self.save_brush_size = 5
+        self.save_brush_color = QColor(0, 0, 0)
+        self.save_point = 5
         self.tool()
 
 
@@ -19,13 +22,14 @@ class ToolUI(QWidget):
         layout = QHBoxLayout()
         self.setLayout(layout)
 
-        self.tools = [ Button('{}'.format(str(x)),self.buttonClicked) for x in range(5)]
+        self.tools = [ Button('{}'.format(str(x)), self.buttonClicked) for x in range(4)]
 
-        self.tools.append(Button('빨강펜',self.buttonClicked))
-        self.tools.append(Button('파랑펜',self.buttonClicked))
+        self.tools.append(Button('그리기', self.buttonClicked))
+        self.tools.append(Button('직선', self.buttonClicked))
+        self.tools.append(Button('지우개', self.buttonClicked))
 
-        self.tools.append(Button('저장',self.save))
-        self.tools.append(Button('지우기',self.clear))
+        self.tools.append(Button('저장', self.save))
+        self.tools.append(Button('지우기', self.clear))
 
         save_action = QAction('Save', self)
         save_action.setShortcut('Ctrl+S')
@@ -41,31 +45,42 @@ class ToolUI(QWidget):
             layout.addWidget(self.tools[i])
         layout.addStretch()
 
-        #색상
-        layout.addWidget(QLabel('전경색: '))
-        #layout.addWidget(Button('1', self.foregroundColor()))
-        layout.addWidget(QLabel('배경색: '))
-        #layout.addWidget(Button('2',self.backgroungColor()))
 
+        #색상         >> 메시지 남김 : 원래 전경색이었는데 색 바꾸기로 그냥 이름 바꿨어!
+        layout.addWidget(QLabel('색 바꾸기: '))
+        #layout.addWidget(Button('1', self.foregroundColor()))
+        #layout.addWidget(QLabel('배경색: '))
+        #layout.addWidget(Button('배경', self.buttonClicked()))
+        #layout.addWidget(QLabel('현재 색깔: '))
+
+
+
+# ------------------------------------------------------------------------ #
+
+
+    # 그림 저장하기
     def save(self):
         fpath, _ = QFileDialog.getSaveFileName(self, 'Save Image', '', "PNG(*.png) ")
-
         if fpath:
             self.canvas.image.save(fpath + '.png')
-            # 혹시 다른 확장자로 바꾸거나 .png.png 로 저장되는 걸 수정하고 싶으면 이 부분 고치
+            # 혹시 다른 확장자로 바꾸거나 .png.png 로 저장되는 걸 수정하고 싶으면 이 부분 고치기
 
 
+    # 그림 초기화
     def clear(self):
         self.canvas.image.fill(Qt.white)
-        self.update()
+        self.canvas.update()
 
 
     #def foregroundColor(self):
-    #    Canvas.ChangedColor(Qt.red)
+    #    self.ChangedColor(Qt.red)
 
+    # 배경색 (바뀌게 될지도 모르니까 그냥 냅둠)
     #def backgroungColor(self):
-    #    Canvas.ChangedColor(Qt.white)
+    #    self.ChangedColor(QColor(255,255,255))
 
+
+    # 색깔과 사이즈에 변화주는 함수
     def ChangedColor(self, color):
         self.canvas.brush_color = color
 
@@ -73,13 +88,79 @@ class ToolUI(QWidget):
         self.canvas.brush_size = size
 
 
+    # 직선긋기
+    def StraightLine(self):
+        pass
+
+
+
+# ------------------------------------------------------------------- #
+
 
     def buttonClicked(self):
         button = self.sender()
         key = button.text()
 
-        if key=='빨강펜':
-            self.ChangedColor(Qt.red)
+        if key == '그리기':
+            self.canvas.save_drawingType = 'drawing'
+            self.canvas.brush_size = self.save_brush_size
+            self.save_brush_size = self.change('선 굵기', '50', self.save_brush_size)
+            self.ChangedSize(self.save_brush_size)
+            self.ChangedColor(self.save_brush_color)
 
-        elif key=='파랑펜':
-            self.ChangedColor(Qt.blue)
+        elif key == '지우개':
+            self.canvas.save_drawingType = 'drawing'
+            self.ChangedColor(QColor(255, 255, 255))
+            self.save_eraser_size = self.change('지우개 굵기', '50', self.save_eraser_size)
+            self.ChangedSize(self.save_eraser_size)
+
+        elif key == '직선':
+            self.canvas.save_drawingType = 'line'
+            self.StraightLine()
+
+
+
+    # 굵기 정하는 창 띄워주는 함수
+    def Thickness(self, thicknessType, maxThickness, saveValue): # (원하는 굵기 종류, 굵기의 최대 범위, cancel 시 반환할 값)
+        while True:
+            thickness, ok = QInputDialog.getText(self, '{}'.format(thicknessType),
+                                            '원하는 굵기를 선택하세요.\n최대 굵기: {}'.format(maxThickness))
+            if ok == False:
+                return saveValue
+            if thickness == '':
+                QMessageBox.warning(self, '경고!', '숫자를 입력하세요.', QMessageBox.Ok)
+                continue
+            elif thickness.isalnum():
+                pass
+
+            thickness = int(thickness)
+
+            if thickness <= 0:  # 예외처리
+                QMessageBox.warning(self, '경고!', '0 보다 큰 수를 입력하세요.', QMessageBox.Ok)
+                continue
+            elif thickness > int(maxThickness):
+                QMessageBox.warning(self, '경고!', '{} 보다 작은 수를 입력하세요.'.format(maxThickness), QMessageBox.Ok)
+                continue
+            else:
+                if ok:
+                    return thickness
+
+
+    # 변경할 것인지 물어보는 창 띄워주는 함수
+    def change(self, changingType, limit, saveValue):
+        result = saveValue
+        answer = QMessageBox.question(self, '질문', '{}를 변경할까요?'.format(changingType),
+                                          QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if answer == QMessageBox.Yes:
+            self.save_point = self.Thickness('{}'.format(changingType),
+                                                       '{}'.format(limit), saveValue)
+            result = self.save_point
+        else:
+            pass
+
+        return result
+
+
+    # 현재 무슨 색인지 텍스트로 알려주기
+    def nowColor(self):
+        pass
