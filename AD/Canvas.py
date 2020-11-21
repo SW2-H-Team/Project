c44c70317933aca472e56f9edfb63002692c4012
Kim-Yul <65997015+Kim-Yul@users.noreply.github.com>
2020-11-22 03:16:43 +0900

    그리기 모드를 나눌 수 있도록 수정
    기존에 그림을 그릴 수 있던 것에서 직선도 구현할 수 있도록 틀을 수정함

--- AD/Canvas.py
+++ AD/Canvas.py
@@ -6,10 +6,10 @@ from PyQt5.QtCore import *
 
 class Canvas(QMainWindow):
 
-    def __init__(self,painter):
+    def __init__(self, painter):
         super().__init__()
 
-        self.painter=painter
+        self.painter = painter
 
         self.image = QImage(QSize(725, 430), QImage.Format_RGB32)
         self.image.fill(Qt.white)
@@ -18,8 +18,12 @@ class Canvas(QMainWindow):
         self.brush_color = Qt.black
         self.brush_size = 5
 
-        #self.ChangedSize(5)
+        self.save_drawingType = 'drawing'
+
+
         self.last_point = QPoint()
+
+
         self.initUI()
 
 
@@ -27,32 +31,24 @@ class Canvas(QMainWindow):
 
 
     def initUI(self):
-
-        #menubar = self.menuBar()
-        #menubar.setNativeMenuBar(False)
-        #filemenu = menubar.addMenu('File')
-
-
-
-        #tools = ToolUI(self)
-        #tools.tools[7].addAction(save_action)
-        #tools.tools[8].addAction(clear_action)
-
-        #filemenu.addAction(save_action)
-        #filemenu.addAction(clear_action)
-
         self.setWindowTitle('Simple Painter')
         self.setGeometry(300, 300, 725, 430)
         self.show()
+
 
     def paintEvent(self, e):
         canvas = QPainter(self)
         canvas.drawImage(self.rect(), self.image, self.image.rect())
 
+
     def mousePressEvent(self, e):
         if e.button() == Qt.LeftButton:
-            self.drawing = True
-            self.last_point = e.pos()
+            if self.save_drawingType == 'drawing':
+                self.drawing = True
+                self.last_point = e.pos()
+            elif self.save_drawingType == 'line':
+                pass
+
 
     def mouseMoveEvent(self, e):
         if (e.buttons() & Qt.LeftButton) & self.drawing:
@@ -60,11 +56,22 @@ class Canvas(QMainWindow):
             painter.setPen(QPen(self.brush_color, self.brush_size, Qt.SolidLine, Qt.RoundCap))
             painter.drawLine(self.last_point, e.pos())
             self.last_point = e.pos()
+
+            if self.save_drawingType == 'drawing':
+                pass
+            elif self.save_drawingType == 'line':
+                pass
+
             self.update()
+
 
     def mouseReleaseEvent(self, e):
         if e.button() == Qt.LeftButton:
-            self.drawing = False
+            if self.save_drawingType == 'drawing':
+                self.drawing = False
+            elif self.save_drawingType == 'line':
+                pass
+
 
 
 
