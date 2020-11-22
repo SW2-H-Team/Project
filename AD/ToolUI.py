db3493ff4ead1e5213f390b2c03c83a8493b5dc8
Kim-Yul <65997015+Kim-Yul@users.noreply.github.com>
2020-11-22 20:00:34 +0900

    선모드 추가
    선모드를 추가하고 앞으로 해야하는 것의 틀만 잡음

--- AD/ToolUI.py
+++ AD/ToolUI.py
@@ -12,20 +12,25 @@ class ToolUI(QWidget):
         self.painter = painter
         self.save_eraser_size = 5
         self.save_brush_size = 5
+        self.save_line_size = 5
+        self.save_brush_mode = Qt.SolidLine
         self.save_brush_color = QColor(0, 0, 0)
         self.save_point = 5
         self.tool()
-
 
 
     def tool(self):
         layout = QHBoxLayout()
         self.setLayout(layout)
 
-        self.tools = [ Button('{}'.format(str(x)), self.buttonClicked) for x in range(4)]
+        #self.tools = [Button('{}'.format(str(x)), self.buttonClicked) for x in range(2)]
+        self.tools = []
 
         self.tools.append(Button('그리기', self.buttonClicked))
         self.tools.append(Button('직선', self.buttonClicked))
+        self.tools.append(Button('선모드', self.buttonClicked))
+        self.tools.append(Button('색효과', self.buttonClicked))
+        self.tools.append(Button('스포이드', self.buttonClicked))
         self.tools.append(Button('지우개', self.buttonClicked))
 
         self.tools.append(Button('저장', self.save))
@@ -39,20 +44,17 @@ class ToolUI(QWidget):
         clear_action.setShortcut('Ctrl+C')
         clear_action.triggered.connect(self.clear)
 
-
-
         for i in range(len(self.tools)):
             layout.addWidget(self.tools[i])
         layout.addStretch()
 
 
-        #색상         >> 메시지 남김 : 원래 전경색이었는데 색 바꾸기로 그냥 이름 바꿨어!
+        # 색상         >> 메시지 남김 : 원래 전경색이었는데 색 바꾸기로 그냥 이름 바꿨어!
         layout.addWidget(QLabel('색 바꾸기: '))
-        #layout.addWidget(Button('1', self.foregroundColor()))
-        #layout.addWidget(QLabel('배경색: '))
-        #layout.addWidget(Button('배경', self.buttonClicked()))
-        #layout.addWidget(QLabel('현재 색깔: '))
-
+        # layout.addWidget(Button('1', self.foregroundColor()))
+        # layout.addWidget(QLabel('배경색: '))
+        # layout.addWidget(Button('배경', self.buttonClicked()))
+        # layout.addWidget(QLabel('현재 색깔: '))
 
 
 # ------------------------------------------------------------------------ #
@@ -75,7 +77,7 @@ class ToolUI(QWidget):
             filenameExtensionLen = len(fpath)
             _ = fpath[filenameExtension:filenameExtensionLen]
             fpath = fpath[0:filenameExtension]
-            
+
         # 파일 저장하기
         if fpath:
             self.canvas.image.save(fpath + _)
@@ -87,11 +89,11 @@ class ToolUI(QWidget):
         self.canvas.update()
 
 
-    #def foregroundColor(self):
+    # def foregroundColor(self):
     #    self.ChangedColor(Qt.red)
 
     # 배경색 (바뀌게 될지도 모르니까 그냥 냅둠)
-    #def backgroungColor(self):
+    # def backgroungColor(self):
     #    self.ChangedColor(QColor(255,255,255))
 
 
@@ -102,14 +104,17 @@ class ToolUI(QWidget):
     def ChangedSize(self, size):
         self.canvas.brush_size = size
 
+    def ChangedValue(self, color, size):
+        self.ChangedColor(color)
+        self.ChangedSize(size)
 
-    # 직선긋기
-    def StraightLine(self):
+
+    def spoid(self, e):
         pass
 
 
 
-# ------------------------------------------------------------------- #
+# ----------------------------------------------------------------------- #
 
 
     def buttonClicked(self):
@@ -120,33 +125,48 @@ class ToolUI(QWidget):
             self.canvas.save_drawingType = 'drawing'
             self.canvas.brush_size = self.save_brush_size
             self.save_brush_size = self.change('선 굵기', '50', self.save_brush_size)
-            self.ChangedSize(self.save_brush_size)
-            self.ChangedColor(self.save_brush_color)
+            self.ChangedValue(self.save_brush_color, self.save_brush_size)
 
         elif key == '지우개':
             self.canvas.save_drawingType = 'drawing'
-            self.ChangedColor(QColor(255, 255, 255))
             self.save_eraser_size = self.change('지우개 굵기', '50', self.save_eraser_size)
-            self.ChangedSize(self.save_eraser_size)
+            self.ChangedValue(QColor(255, 255, 255), self.save_eraser_size)
 
         elif key == '직선':
             self.canvas.save_drawingType = 'line'
-            self.StraightLine()
+            self.canvas.brush_size = self.save_line_size
+            self.save_line_size = self.change('직선 굵기', '50', self.save_line_size)
+            self.ChangedValue(self.save_brush_color, self.save_line_size)
+
+        elif key == '선모드':
+            self.canvas.brush_mode = self.save_brush_mode
+            self.save_brush_mode = self.changingMode('선의 모드', '4', self.save_brush_mode)
+            self.canvas.brush_mode = self.save_brush_mode
+
+        elif key == '색효과':
+            pass
+
+        elif key == '스포이드':
+            pass
+
 
 
 
     # 굵기 정하는 창 띄워주는 함수
-    def Thickness(self, thicknessType, maxThickness, saveValue): # (원하는 굵기 종류, 굵기의 최대 범위, cancel 시 반환할 값)
+    def Thickness(self, thicknessType, maxThickness, saveValue):  # (원하는 굵기 종류, 굵기의 최대 범위, cancel 시 반환할 값)
         while True:
             thickness, ok = QInputDialog.getText(self, '{}'.format(thicknessType),
-                                            '원하는 굵기를 선택하세요.\n최대 굵기: {}'.format(maxThickness))
+                                                 '원하는 굵기를 선택하세요.\n최대 굵기: {}'.format(maxThickness))
             if ok == False:
                 return saveValue
             if thickness == '':
                 QMessageBox.warning(self, '경고!', '숫자를 입력하세요.', QMessageBox.Ok)
                 continue
-            elif thickness.isalnum():
+            elif thickness.isdigit():
                 pass
+            else:
+                QMessageBox.warning(self, '경고!', '값이 이상합니다. 다시 입력해주세요.', QMessageBox.Ok)
+                continue
 
             thickness = int(thickness)
 
@@ -159,6 +179,7 @@ class ToolUI(QWidget):
             else:
                 if ok:
                     return thickness
+
 
 
     # 변경할 것인지 물어보는 창 띄워주는 함수
@@ -174,6 +195,34 @@ class ToolUI(QWidget):
             pass
 
         return result
+
+
+    # 선모드 선택 
+    def changingMode(self, changingType, limit, saveValue):
+        text = '원하는 모드를 선택하세요.\n0 : 실선\n1 : 짧은실선\n2 : 점선\n3 : 실점선'
+        list = [Qt.SolidLine, Qt.DashLine, Qt.DotLine, Qt.DashDotLine, ]
+        result = saveValue
+        answer = QMessageBox.question(self, '질문', '{}를 변경할까요?'.format(changingType),
+                                      QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+
+        if answer == QMessageBox.Yes:
+            while True:
+                thickness, ok = QInputDialog.getInt(self, '{}'.format(changingType), text)
+                if ok == False:
+                    break
+                elif thickness > 3:
+                    QMessageBox.warning(self, '경고!', '{} 보다 작은 수를 입력하세요.'.format(limit), QMessageBox.Ok)
+                    continue
+                else:
+                    result = list[thickness]
+                    break
+        else:
+            pass
+
+        return result
+
+
+
 
 
     # 현재 무슨 색인지 텍스트로 알려주기
