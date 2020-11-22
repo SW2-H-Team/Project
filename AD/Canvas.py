60bf45d572584fcdef3f38821fb84471a5da3397
Kim-Yul <65997015+Kim-Yul@users.noreply.github.com>
2020-11-22 15:53:25 +0900

    직선 모드를 추가함
    직선긋기를 사용할 수 있게 canvas 내에 직선 모드 추가

--- AD/Canvas.py
+++ AD/Canvas.py
@@ -1,3 +1,4 @@
+
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
@@ -18,9 +19,15 @@ class Canvas(QMainWindow):
         self.brush_color = Qt.black
         self.brush_size = 5
 
+        # 모드 선택 ['drawing':그리기, 'line':직선, ]
         self.save_drawingType = 'drawing'
 
 
+        # 직선 모드에서 활용할 point
+        self.past_point = QPoint()
+        self.present_point = QPoint()
+
+        # 그리기 모드에서 활용할 point
         self.last_point = QPoint()
 
 
@@ -41,36 +48,44 @@ class Canvas(QMainWindow):
         canvas.drawImage(self.rect(), self.image, self.image.rect())
 
 
+    # 좌클릭을 눌렀을 때
     def mousePressEvent(self, e):
         if e.button() == Qt.LeftButton:
             if self.save_drawingType == 'drawing':
                 self.drawing = True
                 self.last_point = e.pos()
             elif self.save_drawingType == 'line':
-                pass
+                self.drawing = True
+                self.past_point = e.pos()
 
 
+    # 좌클릭을 한 상태로 움직일 때
     def mouseMoveEvent(self, e):
         if (e.buttons() & Qt.LeftButton) & self.drawing:
             painter = QPainter(self.image)
             painter.setPen(QPen(self.brush_color, self.brush_size, Qt.SolidLine, Qt.RoundCap))
-            painter.drawLine(self.last_point, e.pos())
-            self.last_point = e.pos()
 
             if self.save_drawingType == 'drawing':
-                pass
+                painter.drawLine(self.last_point, e.pos())
+                self.last_point = e.pos()
             elif self.save_drawingType == 'line':
                 pass
 
             self.update()
 
 
+    # 좌클릭 버튼에서 손이 떼졌을 때
     def mouseReleaseEvent(self, e):
         if e.button() == Qt.LeftButton:
             if self.save_drawingType == 'drawing':
                 self.drawing = False
             elif self.save_drawingType == 'line':
-                pass
+                painter = QPainter(self.image)
+                painter.setPen(QPen(self.brush_color, self.brush_size, Qt.SolidLine, Qt.RoundCap))
+                self.present_point = e.pos()
+                painter.drawLine(self.past_point, self.present_point)
+                self.update()
+                self.drawing = False
 
 
 
@@ -78,6 +93,7 @@ class Canvas(QMainWindow):
 
 
 if __name__ == '__main__':
+
     app = QApplication(sys.argv)
-    canvas = Canvas()
+    canvas = Canvas('heng')
     sys.exit(app.exec_())
