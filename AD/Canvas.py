bb4f786b1467b657e0f6b9f3f8403dcb8d4813ec
Kim-Yul <65997015+Kim-Yul@users.noreply.github.com>
2020-11-24 19:20:58 +0900

    image -> pixmap 변경
    기존의 작동 방식을 완전히 변경

--- AD/Canvas.py
+++ AD/Canvas.py
@@ -11,34 +11,42 @@ class Canvas(QMainWindow):
 
         self.status = status
 
-        self.image = QImage(QSize(725, 430), QImage.Format_RGB32)
-        self.image.fill(Qt.white)
+        self.drawingPath = None
+
+        mainImage = QImage(QSize(725, 430), QImage.Format_RGB32)
+        mainImage.fill(Qt.white)
+        mainImage.save("main_image.png")
+
+        # image 불러오기
+        self.image = QPixmap("main_image.png")
+        self.resize(self.image.width(),self.height())
+
         self.drawing = False
 
-        self.brush_color = QColor(0, 0, 0)
+        self.color_r = 0
+        self.color_g = 0
+        self.color_b = 0
+        self.color_a = 255
+        self.brush_color = QColor(self.color_r, self.color_g, self.color_b, self.color_a)
         self.brush_size = 5
         self.brush_mode = Qt.SolidLine
+
 
         self.string = 'you and me'
         self.stringFont = 'DejaVu Sans Mono'
         self.stringFontSize = 18
 
 
-       # 모드 선택 ['drawing':그리기, 'line':직선, 'text':텍스트]
+       # 모드 선택 ['drawing':그리기, 'line':직선, 'text':텍스트, ]
         self.save_drawingType = 'drawing'
 
 
         # 직선 모드에서 활용할 point
-        self.past_point = QPoint()
-        self.present_point = QPoint()
-
-        # 그리기 모드에서 활용할 point
-        self.last_point = QPoint()
+        self.past_point = None
+        self.present_point = None
 
 
         self.initUI()
-
-
 
 
 
@@ -50,69 +58,53 @@ class Canvas(QMainWindow):
 
     def paintEvent(self, e):
         canvas = QPainter(self)
-        canvas.drawImage(self.rect(), self.image, self.image.rect())
+        canvas.drawPixmap(self.rect(), self.image, self.rect())
+        if self.drawingPath:
+            canvas.setPen(QPen(self.brush_color, self.brush_size, self.brush_mode, Qt.RoundCap))
+            canvas.drawPath(self.drawingPath)
 
 
     # 좌클릭을 눌렀을 때
     def mousePressEvent(self, e):
         if e.button() == Qt.LeftButton:
+            self.drawingPath = QPainterPath()
+            self.drawingPath.moveTo(e.pos())
             if self.save_drawingType == 'drawing':
-                self.drawing = True
-                self.last_point = e.pos()
-            elif self.save_drawingType == 'line':
-                self.drawing = True
+                pass
+            else:
                 self.past_point = e.pos()
-            elif self.save_drawingType == 'text':
-                self.drawing = True
-                self.last_point = e.pos()
 
 
     # 좌클릭을 한 상태로 움직일 때
     def mouseMoveEvent(self, e):
-        if (e.buttons() & Qt.LeftButton) & self.drawing:
+        if e.buttons() and Qt.LeftButton and self.drawingPath:
             painter = QPainter(self.image)
             painter.setPen(QPen(self.brush_color, self.brush_size, self.brush_mode, Qt.RoundCap))
-
-            if self.save_drawingType == 'drawing':
-                painter.drawLine(self.last_point, e.pos())
-                self.last_point = e.pos()
-            elif self.save_drawingType == 'line':
-                if self.brush_size <= 3:
-                    painter.setPen(QPen(Qt.gray, 1))
-                else:
-                    painter.setPen(QPen(Qt.gray, self.brush_size // 3, self.brush_mode, Qt.RoundCap))
-                painter.drawPoint(self.past_point)
-            elif self.save_drawingType == 'text':
+            if self.save_drawingType == 'text':
                 pass
-
-            self.update()
+            else:
+                self.drawingPath.lineTo(e.pos())
+                self.update()
 
 
     # 좌클릭 버튼에서 손이 떼졌을 때
     def mouseReleaseEvent(self, e):
-        if e.button() == Qt.LeftButton:
-            if self.save_drawingType == 'drawing':
-                self.drawing = False
+        if e.button() == Qt.LeftButton and self.drawingPath:
+            painter = QPainter(self.image)
+            painter.setPen(QPen(self.brush_color, self.brush_size, self.brush_mode, Qt.RoundCap))
+
+            if self.save_drawingType == ('drawing'):
+                painter.drawPath(self.drawingPath)
             elif self.save_drawingType == 'line':
-                painter = QPainter(self.image)
-                painter.setPen(QPen(self.brush_color, self.brush_size, self.brush_mode, Qt.RoundCap))
                 self.present_point = e.pos()
                 painter.drawLine(self.past_point, self.present_point)
-                self.update()
-                self.drawing = False
             elif self.save_drawingType == 'text':
-                painter = QPainter(self.image)
-                self.draw_text(painter)
-                self.drawing = False
+                painter.setPen(self.brush_color)
+                painter.setFont(QFont(self.stringFont, self.stringFontSize))
+                painter.drawText(self.past_point, self.string)
 
-
-
-    def draw_text(self, qp):
-        qp.setPen(self.brush_color)
-        qp.setFont(QFont(self.stringFont, self.stringFontSize))
-        qp.drawText(self.last_point, self.string)
-        self.update()
-
+            self.drawingPath = None
+            self.update()
 
 
 
